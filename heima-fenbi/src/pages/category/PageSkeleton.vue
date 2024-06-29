bfcedf4f04655d03ea5183a9d9317d323c1a57e5
JackeyLove1 <1572727410@qq.com>
2024-06-29 19:20:37 +0800

    feat: uni fenbi v1

---
+++ heima-fenbi/src/pages/category/PageSkeleton.vue
@@ -0,0 +1,131 @@
+<template name="skeleton">
+  <view class="sk-container">
+    <view class="viewport">
+      <view class="search">
+        <view class="input">
+          <text class="icon-search sk-transparent sk-text-14-2857-711 sk-text sk-pseudo sk-pseudo-circle">女靴</text>
+        </view>
+      </view>
+      <view class="categories">
+        <scroll-view scroll-y="true" class="primary">
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-770 sk-text">居家</text>
+          </view>
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-64 sk-text">美食</text>
+          </view>
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-242 sk-text">服饰</text>
+          </view>
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-523 sk-text">母婴</text>
+          </view>
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-975 sk-text">个护</text>
+          </view>
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-328 sk-text">严选</text>
+          </view>
+          <view class="item active sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-477 sk-text">数码</text>
+          </view>
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-230 sk-text">运动</text>
+          </view>
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-384 sk-text">杂项</text>
+          </view>
+        </scroll-view>
+        <scroll-view scroll-y="true" class="secondary">
+          <view is="components/XtxSwiper" class="banner">
+            <view class="carousel XtxSwiper--carousel">
+              <swiper circular="true" interval="1000" current="0" autoplay="false">
+                <swiper-item style="position: absolute; width: 100%; height: 100%; transform: translate(0%, 0px) translateZ(0px);">
+                  <navigator class="navigator XtxSwiper--navigator" hover-class="none">
+                    <image class="image XtxSwiper--image sk-image" mode="aspectFill"></image>
+                  </navigator>
+                </swiper-item>
+                <swiper-item style="position: absolute; width: 100%; height: 100%; transform: translate(100%, 0px) translateZ(0px);">
+                  <navigator class="navigator XtxSwiper--navigator" hover-class="none">
+                    <image class="image XtxSwiper--image sk-image" mode="aspectFill"></image>
+                  </navigator>
+                </swiper-item>
+              </swiper>
+              <view class="indicator XtxSwiper--indicator">
+                <text class="dot XtxSwiper--dot active XtxSwiper--active"></text>
+                <text class="dot XtxSwiper--dot"></text>
+                <text class="dot XtxSwiper--dot"></text>
+                <text class="dot XtxSwiper--dot"></text>
+                <text class="dot XtxSwiper--dot"></text>
+              </view>
+            </view>
+          </view>
+          <view class="panel">
+            <view class="title">
+              <text class="name sk-transparent sk-text-27-2727-78 sk-text">影音娱乐</text>
+              <navigator class="more sk-transparent sk-text-30-3030-165 sk-text sk-pseudo sk-pseudo-circle" hover-class="none">全部</navigator>
+            </view>
+            <view class="section">
+              <navigator class="goods" hover-class="none">
+                <image class="image sk-image"></image>
+                <view class="name ellipsis sk-transparent sk-text-14-2857-738 sk-text">airline真无线耳机，空气感佩戴，全新升级</view>
+                <view class="price">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="number sk-transparent sk-text-14-2857-18 sk-text">189.00</text>
+                </view>
+              </navigator>
+              <navigator class="goods" hover-class="none">
+                <image class="image sk-image"></image>
+                <view class="name ellipsis sk-transparent sk-text-14-2857-385 sk-text">善谋者得天下，三国杀桌游</view>
+                <view class="price">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="number sk-transparent sk-text-14-2857-893 sk-text">158.00</text>
+                </view>
+              </navigator>
+              <navigator class="goods" hover-class="none">
+                <image class="image sk-image"></image>
+                <view class="name ellipsis sk-transparent sk-text-14-2857-78 sk-text">平台云音乐氧气立体声蓝牙头戴式触控耳机</view>
+                <view class="price">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="number sk-transparent sk-text-14-2857-221 sk-text">459.00</text>
+                </view>
+              </navigator>
+              <navigator class="goods" hover-class="none">
+                <image class="image sk-image"></image>
+                <view class="name ellipsis sk-transparent sk-text-14-2857-245 sk-text">发现隐藏角落的摄像头，多功能红外探测仪</view>
+                <view class="price">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="number sk-transparent sk-text-14-2857-407 sk-text">99.00</text>
+                </view>
+              </navigator>
+              <navigator class="goods" hover-class="none">
+                <image class="image sk-image"></image>
+                <view class="name ellipsis sk-transparent sk-text-14-2857-70 sk-text">平台云音乐真无线蓝牙耳机Lite版</view>
+                <view class="price">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="number sk-transparent sk-text-14-2857-823 sk-text">179.00</text>
+                </view>
+              </navigator>
+              <navigator class="goods" hover-class="none">
+                <image class="image sk-image"></image>
+                <view class="name ellipsis sk-transparent sk-text-14-2857-449 sk-text">久坐办公易肥胖，电动站立升降桌，带APP</view>
+                <view class="price">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="number sk-transparent sk-text-14-2857-118 sk-text">999.00</text>
+                </view>
+              </navigator>
+              <navigator class="goods" hover-class="none">
+                <image class="image sk-image"></image>
+                <view class="name ellipsis sk-transparent sk-text-14-2857-531 sk-text">平台云音乐氧气有线入耳式耳机</view>
+              </navigator>
+              <navigator class="goods" hover-class="none">
+                <image class="image sk-image"></image>
+                <view class="name ellipsis sk-transparent sk-text-14-2857-118 sk-text">平台智造零感蓝牙耳机</view>
+              </navigator>
+            </view>
+          </view>
+        </scroll-view>
+      </view>
+    </view>
+  </view>
+</template>
---
+++ heima-fenbi/src/pages/index/index.vue
@@ -0,0 +1,112 @@
+<script setup lang="ts">
+
+import CustomNavbar from "@/pages/index/components/CustomNavbar.vue";
+import {onLoad} from '@dcloudio/uni-app'
+import {getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI} from "@/services/home";
+import {ref} from "vue";
+import type {BannerItem, CategoryItem, HotItem} from "@/types/home";
+import CategoryPanel from "@/pages/index/components/CategoryPanel.vue";
+import HotPanel from "@/pages/index/components/HotPanel.vue";
+import XtxGuess from "@/components/XtxGuess.vue";
+import type {XtxGuessInstance} from "@/components/components";
+import PageSkeleton from "@/pages/index/PageSkeleton.vue";
+
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+  console.log("bannerList: ", bannerList)
+}
+
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+  console.log("categoryList: ", categoryList.value)
+}
+
+const hoList = ref<HotItem[]>([])
+const getHomeHotData = async () => {
+  const res = await getHomeHotAPI()
+  hoList.value = res.result
+  console.log("hoList: ", hoList)
+}
+
+const guessLikeRef = ref<XtxGuessInstance>()
+
+const getHomePageData = async () => {
+  await Promise.all([
+    getHomeBannerData(),
+    getHomeCategoryData(),
+    getHomeHotData()
+  ])
+}
+
+const isLoading = ref(false)
+onLoad(async () => {
+  isLoading.value = true
+  await getHomePageData()
+  isLoading.value = false
+})
+
+const isTriggered = ref(false)
+const onRefreshTriggered = async () => {
+  isTriggered.value = true
+  isLoading.value = true
+  await getHomePageData()
+  guessLikeRef.value?.resetGuessPageData()
+  isLoading.value = false
+  isTriggered.value = false
+}
+
+const onScrollToLower = async () => {
+  console.log("guessRef: ", guessLikeRef.value)
+  await guessLikeRef.value?.getMore()
+}
+
+</script>
+
+<template>
+  <!-- 顶部导航栏 -->
+  <CustomNavbar/>
+
+  <scroll-view
+    enable-back-to-top
+    refresher-enabled
+    :refresher-triggered="isTriggered"
+    @refresherrefresh="onRefreshTriggered"
+    class="scroll-view"
+    @scrolltolower="onScrollToLower"
+    scroll-y
+  >
+    <PageSkeleton v-if="isLoading"/>
+    <template v-else>
+      <!-- 轮播图组件 -->
+      <XtxSwiper :list="bannerList"/>
+
+      <!-- 首页分类组件 -->
+      <CategoryPanel :list="categoryList"/>
+
+      <!-- 推荐导航栏 -->
+      <HotPanel :list="hoList"/>
+
+      <!-- 猜你喜欢 -->
+      <XtxGuess ref="guessLikeRef"/>
+    </template>
+
+  </scroll-view>
+
+</template>
+
+<style lang="scss">
+page {
+  background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  .scroll-view {
+    flex: 1;
+  }
+}
+</style>
